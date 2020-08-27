d457b3ce0dcdb9634535291dd4a7b96079a3874a
HellFire <vaxure1337@gmail.com>
2020-08-27 19:58:09 -0230

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,17 @@
 local PlayerName = game.Players.LocalPlayer.Name;
 
+function DeleteStuff()
+    game:GetService("Players")[PlayerName].PlayerGui.MainGui.Effects:Destroy()
+end
+
 function Coins()
     for i,v in pairs(game:GetService("Workspace").Coins:GetChildren()) do
-        
+        for inv,val in pairs(v:GetChildren()) do
+            if val.Name == "Coin" then
+                val.CFrame = game.Workspace[PlayerName].HumanoidRootPart.CFrame
+                firetouchinterest(game.Workspace[PlayerName].HumanoidRootPart, game:GetService("Workspace").Coins[v.Name][val.Name], 0)
+            end
+        end
     end
 end
 
@@ -21,8 +30,13 @@ function Portals()
 end
 
 function Main()
+    pcall(DeleteStuff)
     Gamepasses();
     Portals();
+    while wait() do
+        pcall(Coins)
+    end
+
 end
 
 Main()
