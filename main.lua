fad6622bb9bcc8003c60894c9406f52c57e84785
HellFire <vaxure1337@gmail.com>
2020-08-27 19:30:58 -0230

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,14 @@
+local PlayerName = game.Players.LocalPlayer.Name;
+
 function Coins()
     
 end
 
 function Gamepasses()
-    
+    for i,v in pairs(game:GetService("Players")[PlayerName].Gamepasses:GetChildren()) do
+        v.Value = true;
+        print("Gamepasses: "..v.Name.." Value: "..tostring(v.Value))
+    end
 end
 
 function Portals()
